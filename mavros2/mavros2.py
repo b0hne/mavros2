2cf710ac661aed81e0676c7ad2fa53de5d8c5c6f
b0hne <sebastian.benkel@tum.de>
2020-08-18 01:09:48 +0200

    removes old .py

--- mavros2/mavros2.py
+++ mavros2/mavros2.py
@@ -32,12 +32,6 @@ class Mav:
             encoding='utf-8',
             write_through=True
         )
-        # self.stderr = TextIOWrapper(
-        #     self.proc.stderr,
-        #     encoding='utf-8',
-        #     write_through=True
-        # )
-
 
 class Mavros2Read(Node):
     def __init__(self, mav, name=None):
@@ -47,26 +41,21 @@ class Mavros2Read(Node):
         self.mav = mav
 
         self.publisher = self.node.create_publisher(String, 'mavros2_output', 10)
-        self.publisher
+        self.publisher # prevent unused variable warning
         
         self.timer = self.create_timer(0.01, self.read_callback)
 
     def read_callback(self):
-        # while rclpy.ok():
-            #activates on newline
-            output = self.mav.stdout.readline()
-            if not not output:
-                print(output)
-                self.publish_msg(output)
-
-        # subscribe
+        output = self.mav.stdout.readline()
+        if not not output:
+            print(output)
+            self.publish_msg(output)
 
     def publish_msg(self, message):
         msg = String()
         if msg is None:
             msg.data = 'empty message'
         else:
-            # print(message)
             msg.data = message
             self.publisher.publish(msg)
                 
@@ -77,35 +66,15 @@ class Mavros2Write(Node):
         self.node = rclpy.create_node(name or type(self).__name__)
 
         self.mav = mav
-    #     self.timer = self.create_timer(1, self.read_callback)
-
-    # def read_callback(self):
-    #     # return
-    #     # print(1)
-    #     line = 'rr{}\n'.format(1)
-    #     print(type(line))
-    #     # self.mav.stdin.write(line)
-    #     print(type('test'))
-    #     self.mav.stdin.write('test\n')
-        # self.mav.stdin.flush()
-
-        # while rclpy.ok():
-            #activates on newline
-            # output = self.mav.stdout.readline()
-
-            # if not not output:
-            #     print(output)
-            #     self.publish_msg(output)
         self.subscription = self.create_subscription(
                                                      String,
                                                      'mavros2',
                                                      self.incomming_msg,
                                                      10)
-        self.subscription  # prevent unused variable warning
+        self.subscription # prevent unused variable warning
 
     def incomming_msg(self, msg):
         print('I heard: "%s"' % msg.data)
-        # print(type(msg.data))
         self.mav.stdin.write(msg.data+'\n')
         self.mav.stdin.flush()
 
